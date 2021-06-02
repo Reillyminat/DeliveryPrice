c8a9e2dd4d2284e75f811cdd68e7d063301b9346
Reillyminat <illyminchik@gmail.com>
2021-06-02 21:40:21 +0300

    Cache realizing (Task 7)

---
+++ AppliancesModel/Models/Cache.cs
@@ -0,0 +1,42 @@
+﻿using AppliancesModel.Contracts;
+using System.Threading;
+
+namespace AppliancesModel.Models
+{
+    public class Cache : ICacheable
+    {
+        private object data;
+
+        private object copy;
+
+        private object locker = new object();
+
+        public Cache(object instance)
+        {
+            data = instance;
+            copy = null;
+        }
+
+        public void AddObject()
+        {
+
+        }
+
+        public T GetObject<T>()
+        {
+            if (copy == null)
+            {
+                lock (locker)
+                {
+                    copy = JsonSerialization.CreateDeepCopy((T)data);
+                }
+                var thread = new Thread(o =>
+                {
+                    Thread.Sleep(10000);
+                    copy = null;
+                });
+            }
+            return (T)copy;
+        }
+    }
+}
