cbe4fba0fd87c0ca0641d38d908459828af415b8
Reillyminat <illyminchik@gmail.com>
2021-06-03 08:56:58 +0300

    Deleted duplicated classes after merge

--- AppliancesModel/Program.cs
+++ AppliancesModel/Program.cs
@@ -8,8 +8,18 @@ namespace AppliancesModel
     {
         static void Main(string[] args)
         {
-            var startup = new AppliancesDistribution();
-            SelectAction(startup);
+            var container = new ImplementationsContainer();
+
+            container.Set<IAppliances>(new Appliances(new List<Appliance>()));
+            var stockInfo = container.Get<IAppliances>();
+            stockInfo.InitializeModel();
+
+            container.Set<IAppliancesDistribution>(new AppliancesDistribution(container.Get<IAppliances>()));
+            var appliancesDistribution = container.Get<IAppliancesDistribution>();
+
+            container.Set<IOutputInputHandler>(new ConsoleInputOutput(container.Get<IAppliancesDistribution>()));
+            var presenter = container.Get<IOutputInputHandler>();
+            presenter.RunMenu(appliancesDistribution);
         }
     }
 }
