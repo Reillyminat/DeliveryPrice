cf91593dfc07ea23636e091af9c0d7466618ee06
Reillyminat <illyminchik@gmail.com>
2021-06-23 18:08:30 +0300

    Not null region check added

--- SQL/SQLQueryNorthwind.sql
+++ SQL/SQLQueryNorthwind.sql
@@ -6,4 +6,4 @@ SELECT * FROM Customers WHERE City='Geneve'
 SELECT LastName, FirstName, BirthDate, Hiredate, Address, City, Region FROM Employees WHERE Region='WA' EXCEPT
 SELECT LastName, FirstName, BirthDate, Hiredate, Address, City, Region FROM Employees WHERE City='Seattle'
 --6
-SELECT Country, Region, COUNT (Region) AS SuppliersCount FROM Suppliers GROUP BY Region, Country HAVING COUNT (Region)>1
+SELECT Country, Region, COUNT (Region) AS SuppliersCount FROM Suppliers WHERE Region IS NOT NULL GROUP BY Region, Country HAVING COUNT (Region)>1
