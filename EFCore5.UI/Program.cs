bb6505dd7d2b1c880382f4e334182f35c79643f0
Reillyminat <illyminchik@gmail.com>
2021-08-16 17:39:05 +0300

    Removed deprecated methods

--- EFCore5.UI/Program.cs
+++ EFCore5.UI/Program.cs
@@ -20,21 +20,5 @@ namespace EFCore5.UI
             consoleIO.UpdateTestData();
             consoleIO.DeleteByIdTestData();
         }
-
-        private static void AddUser()
-        {
-            var usr = new User() { FullName = "Петров Валерий Александрович", Telephone = "380671827384", Address = "г. Новомосковск, ул. Северная, д. 28, кв. 16" };
-            context.Users.Add(usr);
-            context.SaveChanges();
-        }
-        private static void GetUsers()
-        {
-            var users = context.Users.ToList();
-            Console.WriteLine($"Total users {users.Count}");
-            foreach(var user in users)
-            {
-                Console.WriteLine($"Name= {user.FullName}, Id={user.Id}");
-            }
-        }
     }
 }
